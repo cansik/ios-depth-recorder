f26042467f2561aefb63428aa9c959637a5b6376
Florian Bruggisser <florian@bildspur.ch>
2018-08-08 17:26:30 +0200

    tried to add depth capture

--- DepthRecorder/ViewController.swift
+++ DepthRecorder/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Lumina
+import AVKit
 
 class ViewController: UIViewController {
     
@@ -22,6 +23,7 @@ class ViewController: UIViewController {
         
         camera.setCancelButton(visible: false)
         camera.captureDepthData = true
+        camera.streamDepthData = true
         camera.captureLivePhotos = false
         
         present(camera, animated: true, completion:nil)
@@ -40,7 +42,58 @@ extension ViewController : LuminaDelegate {
             // depth data must be manually cast to AVDepthData, as AVDepthData is only available in iOS 11.0 or higher.
             
             //UIImageWriteToSavedPhotosAlbum(stillImage, nil, nil, nil)
+            
+            // save depth image if possible
+            print("trying to save depth image")
+            if #available(iOS 11.0, *) {
+                if let data = depthData as? AVDepthData {
+                    guard let depthImage = data.depthDataMap.normalizedImage(with: controller.position) else {
+                        print("could not convert depth data")
+                        return
+                    }
+                    
+                    print("saving depth image")
+                    CustomPhotoAlbum.shared.save(image: depthImage)
+                }
+                else
+                {
+                    print("data is not depth data")
+                }
+            }
+            else
+            {
+                print("not on ios 11.0")
+            }
+            
+            // save color image
             CustomPhotoAlbum.shared.save(image: stillImage)
+        }
+    }
+}
+
+extension CVPixelBuffer {
+    func normalizedImage(with position: CameraPosition) -> UIImage? {
+        let ciImage = CIImage(cvPixelBuffer: self)
+        let context = CIContext(options: nil)
+        if let cgImage = context.createCGImage(ciImage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(self), height: CVPixelBufferGetHeight(self))) {
+            return UIImage(cgImage: cgImage , scale: 1.0, orientation: getImageOrientation(with: position))
+        } else {
+            return nil
+        }
+    }
+    
+    private func getImageOrientation(with position: CameraPosition) -> UIImageOrientation {
+        switch UIApplication.shared.statusBarOrientation {
+        case .landscapeLeft:
+            return position == .back ? .down : .upMirrored
+        case .landscapeRight:
+            return position == .back ? .up : .downMirrored
+        case .portraitUpsideDown:
+            return position == .back ? .left : .rightMirrored
+        case .portrait:
+            return position == .back ? .right : .leftMirrored
+        case .unknown:
+            return position == .back ? .right : .leftMirrored
         }
     }
 }
